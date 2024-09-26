d59ac64625fe82afe8c3e7724a10e3fa5ec41744
James Xie <hx2227@nyu.edu>
2024-09-26 10:45:09 -0400

    updated

--- rtl/Exercise3.sv
+++ rtl/Exercise3.sv
@@ -7,4 +7,30 @@ module Exercise3 (
     output [15:0] out
 );
 
+wire [7:0] d1;
+wire [7:0] d2;
+
+
+Mystery1 mystery_alpha(
+    .a(a[1:0]),
+    .b(b[7:0]),
+    .c(c[7:0]),
+    .d(d1)
+);
+
+Mystery1 mystery_alpha(
+    .a(a[3:2]),
+    .b(b[15:8]),
+    .c(c[15:8]),
+    .d(d2)
+);
+
+Mystery2 myster_gamma(
+    .clk(clk),
+    .nReset(nReset),
+    .a_in(d1),
+    .b_in(d2),
+    .out(out)
+);
+
 endmodule
--- rtl/Exercise4.sv
+++ rtl/Exercise4.sv
@@ -21,4 +21,17 @@ module Exercise4 (
     output logic [7:0] out
 );
 
+always_comb begin 
+  if (!cs) out = 0;
+  else begin
+    case (sel)
+      2'b00 : out = alpha;
+      2'b01 : out = beta;
+      2'b10 : out = gamma;
+      2'b11 : out = 0;
+    endcase
+  end
+end
+
+
 endmodule
